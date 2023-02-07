c896de4092bc24b52180385909e2488aa4cd101d
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-02-07 09:28:25 +0100

    CarController, CarRequest, CarResponse and CarService added

--- src/main/java/dat3/cars/api/CarController.java
+++ src/main/java/dat3/cars/api/CarController.java
@@ -1,10 +1,12 @@
 package dat3.cars.api;
 
+import dat3.cars.dto.CarRequest;
 import dat3.cars.dto.CarResponse;
+import dat3.cars.dto.MemberRequest;
 import dat3.cars.entity.Car;
 import dat3.cars.service.CarService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +21,30 @@ public class CarController {
   }
 
   //ALL
-  public List<CarResponse> getAll(){carService.;}
+  @GetMapping
+  public List<CarResponse> getCars(){return carService.getCars(false);}
+
+  //ALL
+  @GetMapping
+  public CarResponse getCarById(){return null;}
+
+  //ADMIN
+  @PostMapping()
+  public CarResponse addCar(@RequestBody CarRequest body){
+    return carService.addCar(body);
+  }
+
+  //MEMBER
+  @PutMapping("/{id}")
+  ResponseEntity<Boolean> editCar(@RequestBody MemberRequest body, @PathVariable String id){
+    return null;
+  }
+
+  //ADMIN
+  @PatchMapping("/pricePrDay/{id}/{value}")
+  void setPricePrDay(@PathVariable String id, @PathVariable double value) {}
+
+  //ADMIN
+  @DeleteMapping("/{id}")
+  void deleteMemberByUsername(@PathVariable String id) {}
 }
--- src/main/java/dat3/cars/service/CarService.java
+++ src/main/java/dat3/cars/service/CarService.java
@@ -1,9 +1,12 @@
 package dat3.cars.service;
 
+import dat3.cars.dto.CarRequest;
 import dat3.cars.dto.CarResponse;
 import dat3.cars.entity.Car;
 import dat3.cars.repositories.CarRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -16,11 +19,23 @@ public class CarService {
     this.carRepository = carRepository;
   }
 
-  public List<CarResponse> getAll(boolean includeAll){
+  public List<CarResponse> getCars(boolean includeAll){
     List<Car> cars = carRepository.findAll();
 
     List<CarResponse> carResponses = cars.stream().map(c->new CarResponse(c, includeAll)).toList();
 
     return carResponses;
+  }
+
+  public CarResponse addCar(CarRequest carRequest){
+
+    if(carRepository.existsByModel(carRequest.getModel())){
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Model with this name already exist");
+    }
+
+    Car newCar =  CarRequest.getCarEntity(carRequest);
+    carRepository.save(newCar);
+
+    return new CarResponse(newCar, false);
   }
 }
