e836ee9db98e8e69791367d9c834f65ed6ea58a2
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-02-09 12:32:28 +0100

    index for carList added

--- src/main/java/dat3/cars/api/CarController.java
+++ src/main/java/dat3/cars/api/CarController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/cars")
+@RequestMapping("/api/cars")
 public class CarController {
 
   private CarService carService;
