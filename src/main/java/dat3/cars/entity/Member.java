1653c674ed5f2ea9a1f8b498a97e1d40025f7b60
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-02-17 11:39:30 +0100

    Rettelser i Member og Car

--- src/main/java/dat3/cars/entity/Member.java
+++ src/main/java/dat3/cars/entity/Member.java
@@ -54,7 +54,7 @@ public class Member extends UserWithRoles {
 
   private int ranking;
 
-  @OneToMany//(mappedBy = "member", cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
   private List<Reservation> reservations;
 
   @CreationTimestamp
